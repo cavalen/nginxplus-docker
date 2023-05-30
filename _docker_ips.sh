2ec956245a83791f166e8958e51d57f117f51d8c
cavalen <cavalen@hotmail.com>
2023-05-30 09:17:39 -0400

    Update _docker_ips.sh

--- _docker_ips.sh
+++ _docker_ips.sh
@@ -1,4 +1,1 @@
 docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}} %tab% {{.Name}}' $(docker ps -aq) | sed 's#%tab%#\t#g' | sed 's#/##g' | sort -t . -k 1,1n -k 2,2n -k 3,3n -k 4,4n
-# Rancher Desktop
-docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}} %tab% {{.Name}}' $(docker ps -aq) | sed 's#%tab%#\t#g' | head -n1 | sort -t . -k 1,1n -k 2,2n -k 3,3n -k 4,4n
-
